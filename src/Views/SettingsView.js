3e0e1ac7d7a6a0723460ffe2bbebe23709920a74
2stefant <stefan.lindepil@gmail.com>
2020-11-17 12:16:20 +0100

    Adds .env.example file, adds local storage support

--- src/Views/SettingsView.js
+++ src/Views/SettingsView.js
@@ -6,12 +6,20 @@ import axios from "axios"
 class SettingsView extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: null,
+      email: null
+    }
   }
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/users/1")
     .then(response => {
       console.log(response.data)
+      this.setState({
+        name: response.data.name,
+        email: response.data.email
+      });
     })
     .catch(error => {
       console.log(error)
@@ -22,6 +30,7 @@ class SettingsView extends React.Component {
     return (
       <div>
         <h2>Settings</h2>
+        <span>{this.state.name} - {this.state.email}</span>
         <label>Last used workspace</label>
         <label>Your email address</label>
         <label>Toggl api key</label>
