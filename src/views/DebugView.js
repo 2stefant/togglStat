ab83e28b43222fa08873fb1f51cc10d64e3eba51
Stefan Lindepil <steli18@vgregion.se>
2020-11-22 20:38:24 +0100

    Adds axios example call in DebugView

--- src/views/DebugView.js
+++ src/views/DebugView.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from "axios";
 
 import TogglHack from "../hacks/TogglHack";
 import ErrorBoundary from "../components/ErrorBoundary";
@@ -7,6 +8,9 @@ import ConfigService from "../services/ConfigService";
 const config = ConfigService.getSingleton();
 
 const DebugView = ({ title, description }) => {
+
+  const [axiosData, setAxiosData] = useState(null);
+
   const jsxDebugContent = () => {
     return config.getLocalStorageDefaultValues().debugMode ? (
       <>
@@ -17,16 +21,34 @@ const DebugView = ({ title, description }) => {
     ) : null;
   };
 
+  const axiosCallDemonstration = () => {
+    
+    setAxiosData("...................................");
+
+    axios.get("https://jsonplaceholder.typicode.com/users/1")
+      .then(response => {
+        setAxiosData(`Name: '${response.data.name}', Email: '${response.data.email}'`);
+      })
+      .catch(error => {
+        setAxiosData(`Error: '${error}'`);
+      })
+  }
+
   return (
     <div>
       <h2>{title}</h2>
       <p>{description}</p>
+      <br />
+      <button name="axios" onClick={axiosCallDemonstration}>Axios Demonstration</button>
+      <br />
+      {!axiosData ? null: <label>Axios call result: {axiosData}</label>}
+      <br />
       {!config.getLocalStorageDefaultValues()
-         .debugMode ? null : (
-        <ErrorBoundary>
-          <TogglHack />
-        </ErrorBoundary>
-      )}
+        .debugMode ? null : (
+          <ErrorBoundary>
+            <TogglHack />
+          </ErrorBoundary>
+        )}
     </div>
   );
 };
